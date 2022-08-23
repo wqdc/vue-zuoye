b25fc6d19bdacc43692a98d9c49c4ae6fedcd4f1
dr <1561500402@qq.com>
2022-08-23 21:35:37 +0800

    vue/8.23

---
+++ 8.23/vue-demo/src/App.vue
@@ -0,0 +1,133 @@
+<!-- <template>
+  <div id="app">
+    <ul>
+      <li v-for="(item,index) in arr" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="btn">走一走</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: ["帅哥", "美女", "程序猿"],
+    };
+  },
+  methods: {
+    btn() {
+      // shift() 方法用于把数组的第一个元素从其中删除，并返回第一个元素的值。
+     const newarr = this.arr.shift()
+      this.arr.push(newarr)
+    }
+  },
+};
+</script> -->
+
+<!-- <template>
+  <div id="app">
+    <ul>
+      <li v-for="(item, ind) in arr" :key="ind">
+        <span>{{ item }}</span>
+        <button @click="del(ind)">删除</button>
+      </li>
+    </ul>
+    <button @click="add">生成</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arr: [1, 5, 3],
+    };
+  },
+  methods: {
+    add() {
+     const number = Math.floor(Math.random()*10+1)
+     this.arr.push(number)
+    },
+    del(index) {
+      // console.log(this.arr[index]);
+      this.arr.splice(index,1)
+      // .splice(index,1)
+    }
+  },
+};
+</script> -->
+
+<template>
+  <div id="app">
+    <table class="tb">
+      <tr>
+        <th>编号</th>
+        <th>品牌名称</th>
+        <th>创立时间</th>
+        <th>操作</th>
+      </tr>
+      <!-- 循环渲染的元素tr -->
+      <tr v-for="(item,ind) in list" :key="ind">
+        <td>{{item.id}}</td>
+        <td>{{item.name}}</td>
+        <td>{{item.time}}</td>
+        <td>
+          <button @click="del(ind)">删除</button>
+        </td>
+      </tr>
+      <tr v-if="list.length === 0">
+        <td colspan="4" >没有数据咯~</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        { id: 1, name: "奔驰", time: "2020-08-01" },
+        { id: 2, name: "宝马", time: "2020-08-02" },
+        { id: 3, name: "奥迪", time: "2020-08-03" },
+      ],
+    };
+  },
+  methods:{
+    del(index) {
+      console.log(index);
+      this.list.splice(index,1)
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  width: 600px;
+  margin: 10px auto;
+}
+
+.tb {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+.tb th {
+  background-color: #0094ff;
+  color: white;
+}
+
+.tb td,
+.tb th {
+  padding: 5px;
+  border: 1px solid black;
+  text-align: center;
+}
+
+.add {
+  padding: 5px;
+  border: 1px solid black;
+  margin-bottom: 10px;
+}
+</style>
---
+++ vue.2-作业.md
@@ -0,0 +1,40 @@
+**1. 配置 MiniExtractCssPlugin插件到第一天的作业里 **(1. 锻炼大家查阅百度+查阅官方文档的能力)
+
+- style-loader 的缺陷
+
+
+- 查阅文档了解  https://github.com/webpack-contrib/mini-css-extract-plugin
+
+  ​
+
+**2. webpack思维导图** (掌握webpack)
+
+- 注定: 一定要掌握 plugin和loader的区别(真实的面试题)
+
+
+
+**js题** (1. 熟悉经常使用的语法:reduce 2.为笔试题做准备)
+
+- reduce https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+- 利用reduce 求 1-100的和
+
+
+- 封装一个函数, 利用reduce处理http://www.baidu.com/?name=zhangsan&age=18&sex=nan#, 要求返回一个对象
+  输出的结果 : { name:"zhangsan",age:18,sex:nan }作业
+
+  ```js
+  作业1: 利用reduce 求 1-100的和 (熟悉reduce语法)
+
+  作业2: 笔试原题
+
+  function process( url ){ 
+  	 //.... 使用reduce
+  }
+
+  process(http://www.baidu.com/?name=zhangsan&age=18&sex=nan#) 
+  // 输出:  { name:"zhangsan",age:18,sex:nan }
+  ```
+
+
+
+
